c86bfe19760eb4232552e2ad609376daff80ddf7
Freddy Limpens <freddy.limpens@gmail.com>
2014-12-04 13:07:20 +0100

    small style fixes

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,7 @@ config = {
     templateBaseUrl: '/views/',
     useHtml5Mode: false,
     media_uri: 'http://worldbrain.fr',
-    rest_uri: "http://worldbrain.fr/json",
+    rest_uri: "http://backend.worldbrain.arte.tv/json",
     rootUrl: "http://localhost:8083/",
     applicationAlias : "#/",
     playlist_cluster_order : [2, 16, 17, 3, 11, 18, 8, 7, 4, 14, 12, 5, 15, 13, 9],
